fa40c8feb891fbdf59e99974b6694d213f0d4840
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-04-07 15:42:19 +0900

    firstcommit

---
+++ Music_Diagnosis/Music_DiagnosisTests/Music_DiagnosisTests.swift
@@ -0,0 +1,34 @@
+//
+//  Music_DiagnosisTests.swift
+//  Music_DiagnosisTests
+//
+//  Created by Kazutomo Kita on 2020/04/07.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import XCTest
+@testable import Music_Diagnosis
+
+class Music_DiagnosisTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
